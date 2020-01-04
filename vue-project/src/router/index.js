f7114c29a96bc9cf4784141ad8548aa0b73f8745
张晓鹏 <15538068827@163.com>
2020-01-04 14:16:47 +0800

    add init

---
+++ vue-project/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [{
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/abouta",
+    name: "abouta",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/AboutA.vue")
+  },
+  {
+    path: "/aboutb",
+    name: "aboutb",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/AboutB.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
